4fb9222f1d07cecd0b0b8a9e234c86cd904aea27
张三 <123456@163.com>
2018-06-28 21:30:33 +0800

    add_user_view

--- test_project/manage.py
+++ test_project/manage.py
@@ -6,6 +6,10 @@ app = Flask(__name__）
 def index():
 	return 'index page'
 
+@app.route('/user',methods =['POST','GET'])
+def user():
+	return 'user page'
+
 
 if __name__ == '__main__':
 	app.run()
